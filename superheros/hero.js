a873d14563d41141d804cda01f643241c20bcc84
Timothe Calvin <Tcalv976@gmail.com>
2025-02-10 14:07:13 -0500

    added a working img

--- superheros/hero.js
+++ superheros/hero.js
@@ -1,13 +1,30 @@
 const root = document.getElementById("root");
 
 Hero().then((data) => {
+  Hero().then((data) => {
 
-data.map((Hero) => {
-const div = document.createElement("div")
-div.append(Hero.name)
-div.classList.add("card")
-root.append(div);
+    data.map((Hero) => {
+    const div = document.createElement("div")
+    div.append(Hero.name)
+    div.classList.add("card")
+    root.append(div);
+    })
+   const img = document.createElement('img'); 
+img.src ="https://legendary-digital-network-assets.s3.amazonaws.com/wp-content/uploads/2024/02/24163821/DC-Marvel-Jim-Lee.jpg"; 
+document.body.append(img);
+
+  data.map((Hero) => {
+  const div = document.createElement("div")
+  div.append(Hero.powerstats)
+  div.classList.add("card")
+  root.append(div);
   })
 
+
+
+ 
+
+
+} )
   console.log(data)}).catch((error) => {console.log(error)}); 
 
