e9869aa640680ffd397e71b7ee683214c28e00eb
Timothe Calvin <Tcalv976@gmail.com>
2025-02-11 09:47:05 -0500

    worked on image

--- superheros/hero.js
+++ superheros/hero.js
@@ -8,10 +8,21 @@ Hero().then((data) => {
     div.append(Hero.name)
     div.classList.add("card")
     root.append(div);
-    })
-   const img = document.createElement('img'); 
+
+  const img = document.createElement('img'); 
 img.src ="https://legendary-digital-network-assets.s3.amazonaws.com/wp-content/uploads/2024/02/24163821/DC-Marvel-Jim-Lee.jpg"; 
 document.body.append(img);
+
+    
+    })
+   
+
+data.map((Hero) => {
+  const image = document.createElement('img');
+  image.src = "https://legendary-digital-network-assets.s3.amazonaws.com/wp-content/uploads/2024/02/24163821/DC-Marvel-Jim-Lee.jpg"; 
+  div.append(img);
+})
+
 
   data.map((Hero) => {
   const div = document.createElement("div")
@@ -20,9 +31,6 @@ document.body.append(img);
   root.append(div);
   })
 
-
-
- 
 
 
 } )
